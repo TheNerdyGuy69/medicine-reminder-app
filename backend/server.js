6029ef937405c583511df8a3860406eb965b742b
TheNerdyGuy69 <sahilgadakh72@gmail.com>
2025-07-04 21:42:39 +0530

    Set up Express server with base /api route

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const dotenv = require("dotenv")
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/api", (req,res)=>{
+    res.send("API is running!");
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`);
+    
+});
